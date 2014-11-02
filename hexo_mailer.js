5c2705c00e9209f0ac6dfc1fa8ba4f9ae8b7cc19
Chuck Pierce <chuckmpierce@gmail.com>
2014-11-02 15:32:57 -0500

    Change csvParse function and comment code

--- hexo_mailer.js
+++ hexo_mailer.js
@@ -1,20 +1,24 @@
+//Access Mandrill Mail
 var mandrill = require('mandrill-api/mandrill');
 var mandrill_client = new mandrill.Mandrill('GPFPphz-FLOLFRfDIoQD3g');
 
+//Require Node
 var ejs = require('ejs');
 var fs = require('fs');
 var FeedSub = require('feedsub');
 
+//Parse emails and email template
 var csvFile = fs.readFileSync("friends_list.csv","utf8");
 var emailTemplate = fs.readFileSync('email_template.ejs', 'utf8');
  
- 
+//Access blog content
 var blogContent = new FeedSub('http://chuckpierce.github.io/atom.xml', {
         emitOnStart: true
 });
 
 var latestPosts = [];
 
+//Send email template to contacts 
 function sendEmail(to_name, to_email, from_name, from_email, subject, message_html){
   var message = {
       "html": message_html,
@@ -37,7 +41,6 @@ function sendEmail(to_name, to_email, from_name, from_email, subject, message_ht
   var async = false;
   var ip_pool = "Main Pool";
   mandrill_client.messages.send({"message": message, "async": async, "ip_pool": ip_pool}, function(result) {
-      //console.log(message);
        console.log(result);   
   }, function(e) {
       // Mandrill returns the error as an object with name and message keys
@@ -46,40 +49,33 @@ function sendEmail(to_name, to_email, from_name, from_email, subject, message_ht
   });
   }
 
+//Contacts Parsing function
 function csvParse(csvFile) {
 
-	var csv_data = [];
-	var word = ""
+	var csv_data = csvFile.split("\n");;
+	var personObject;
 
 	var object_array = []
 
-	for(var i = 51; i < csvFile.length; i++) {
-		if(csvFile[i] === "," ) {
-			csv_data.push(word);
-			word = "";
-			continue;
-		}
-		else if(csvFile[i] === "\n") {
-			csv_data.push(word);
-			word = "";
-			var person = new personObject(csv_data);
-			object_array.push(person);
-			csv_data = [];
-		} else {
-			word += csvFile[i];
-		}
-	}
-	return object_array;
+
+   keys = csv_data.shift().split(",");
+ 
+    csv_data.forEach(function(contact){
+        contact = contact.split(",");
+        personObject = {};
+ 
+        for(var i =0; i < contact.length; i++){
+            personObject[keys[i]] = contact[i];
+        }
+ 
+        object_array.push(personObject);
+ 
+    })
+ 
+    return object_array;
 }
 
-function personObject(array) {
-	this.first_name = array[0];
-	this.last_name = array[1];
-	this.months_since_contact = array[2];
-	this.email_address = array[3];
-
-}
-
+//Determine blog posts to put in email template
 blogContent.read(function(err,blogPosts){
     
     blogPosts.forEach(function(post){
@@ -101,31 +97,22 @@ blogContent.read(function(err,blogPosts){
 
 friendList = csvParse(csvFile);
 
-// console.log(friendList);
-
 friendList.forEach(function(row){
  
-    firstName = row["first_name"];
-    numMonthsSinceContact = row["months_since_contact"];
-    emailAddress = row["email_address"];
- 
-    // we make a copy of the emailTemplate variable to a new variable to ensure
-    // we don't edit the original template text since we'll need to us it for 
-    // multiple emails
+    firstName = row["firstName"];
+    numMonthsSinceContact = row["numMonthsSinceContact"];
+    emailAddress = row["emailAddress"];
  
     templateCopy = emailTemplate;
  
-    // use .replace to replace FIRST_NAME and NUM_MONTHS_SINCE_CONTACT with firstName and  monthsSinceLastContact  
     var customizedTemplate = ejs.render(templateCopy, 
                 { firstName: firstName,  
                   monthsSinceContact: numMonthsSinceContact,
                   emailAddress: emailAddress,
                   latestPosts: latestPosts
                 });
- 
-    //console.log(customizedTemplate);
 
-
+  //call sendEmail function
 	sendEmail(firstName, emailAddress, "Chuck", "chuckmpierce@gmail.com", "New Email", customizedTemplate);
     })
  
